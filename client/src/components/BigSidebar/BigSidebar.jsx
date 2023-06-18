080250feadd9d084e1d847bb44c9ca1c13d528fb
hrishik-pocketfm <hrishikrajeshkanade@iitbhilai.ac.in>
2023-06-18 20:33:03 +0530

    ready for deployment

---
+++ client/src/components/BigSidebar/BigSidebar.jsx
@@ -0,0 +1,25 @@
+import { useAppContext } from "../../context/appContext";
+import NavLinks from "../NavLinks";
+import Logo from "../Logo/Logo";
+import Wrapper from "./BigSidebar.styles";
+
+const BigSidebar = () => {
+  const { showSidebar } = useAppContext();
+  return (
+    <Wrapper>
+      <div
+        className={
+          showSidebar ? "sidebar-container " : "sidebar-container show-sidebar"
+        }
+      >
+        <div className="content">
+          <header>
+          </header>
+          <NavLinks />
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default BigSidebar;
